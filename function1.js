66c1912db2500b018fa8e2667b2e361c45e02c41
edmund1000 <edmund1000@naver.com>
2022-10-29 13:26:51 +0900

    코드 만들었음

---
+++ function1.js
@@ -0,0 +1,5 @@
+const jonghyun = 90 % 13;
+console.log(jonghyun);
+
+let addition  = 100000 + 2300233;
+console.log(addition);
---
+++ main.js
@@ -0,0 +1,8 @@
+let student1 = { 
+    koreanScore : 89,
+    englishScore : 80,
+    "science Score" : 90
+};
+console.log(student1.koreanScore);
+console.log(student1["englishScore"]);
+console.log(student1["science Score"]);
